a3ac85d7f0702c01ec8b0cc71153353f8cf9a7ab
jannatnury <jannatnury253@gmail.com>
2022-02-17 19:55:33 +0600

    savings added

--- js/calculation.js
+++ js/calculation.js
@@ -10,19 +10,38 @@ function expansesCalculation() {
     const rentExpance = getExpanses('rent');
     const clothsExpance = getExpanses('cloths');
     const expanceTotal = foodExpance + rentExpance + clothsExpance;
+    // Total Expance
     document.getElementById('expanceTotal').innerText = expanceTotal;
+    // Remaining Balance
     document.getElementById('remainingTotal').innerText = totalIncome - expanceTotal;
+    // savings of total income
+    document.getElementById('')
 
     // function for empty input values
     document.getElementById('foodInput').value = '';
     document.getElementById('rentInput').value = '';
     document.getElementById('clothsInput').value = '';
+    // Erroe message
+    if( isNaN(totalIncome) || totalIncome<0 || isNaN(foodExpance) || foodExpance<0 || isNaN(rentExpance) || rentExpance<0 || isNaN(clothsExpance) || clothsExpance<0){
+        document.getElementById('expanceTotal').innerText = "Invalid Input !";
+        document.getElementById('remainingTotal').innerText = "Invalid Input !";
+    }
 
 }
+// function for Savings button
+function savingsCalculation() {
+    const savingsParcent = document.getElementById('savingInput').value;
+    const totalSavings = (totalIncome * savingsParcent) / 100;
 
-// add eventListner on button
+    // Total savings
+    document.getElementById('savingAmount').innerText = totalSavings;
+}
+
+// add eventListner on calculation button
 document.getElementById('button').addEventListener('click', function () {
     expansesCalculation();
-    console.log('button clicked');
-
 });
+// add eventListener on Save button
+document.getElementById('button-2').addEventListener('click', function () {
+    savingsCalculation();
+})
